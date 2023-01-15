e1fbf3453bcf0714004565d5ef6750eb2e6cafcc
Juan Sebastian Espinola <sebar5er@gmail.com>
2023-01-15 01:29:24 -0300

    add home handler

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,11 +1,28 @@
 import { Head } from '$fresh/runtime.ts';
+import { Handlers, PageProps } from '$fresh/server.ts';
 import Container from '../components/Container.tsx';
 import Header from '../components/Header.tsx';
 import Page from '../components/Page.tsx';
-import Post from '../components/Post.tsx';
+import PostComponent from '../components/Post.tsx';
 import PostsGrid from '../components/PostsGrid.tsx';
 import Title from '../components/Title.tsx';
-export default function Home() {
+import { Post } from '../types/posts.d.ts';
+import { loadPosts } from '../utils/post.ts';
+
+export const handler: Handlers = {
+  async GET(request, context) {
+    console.log('asd');
+
+    const posts = await loadPosts();
+    return context.render({ posts });
+  },
+};
+
+export default function Home(props: PageProps<{ posts: Post[] }>) {
+  const { data } = props;
+  const { posts } = data;
+  console.log(posts);
+
   return (
     <>
       <Head>
@@ -27,12 +44,10 @@ export default function Home() {
           <Container>
             <Title size='5xl'>LATEST</Title>
             <br />
+            {posts.map((post) => <p key={post.title}>{post.title}</p>)}
             <br />
             <PostsGrid>
-              <Post></Post>
-              <Post></Post>
-              <Post></Post>
-              <Post></Post>
+              {posts.map((post) => <PostComponent {...post} />)}
             </PostsGrid>
           </Container>
         </div>
