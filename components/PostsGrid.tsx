7daab03b8af0067246e68cd5f6d58ed99ba6412c
Juan Sebastian Espinola <sebar5er@gmail.com>
2023-01-15 16:09:59 -0300

    add grid reverse variant and change gaps

--- components/PostsGrid.tsx
+++ components/PostsGrid.tsx
@@ -2,17 +2,31 @@ import { FunctionalComponent } from 'preact';
 import { tw } from 'twind';
 import { css } from 'twind/css';
 
-const PostsGrid: FunctionalComponent = ({ children }) => {
+const gridVariants = {
+  default: 1,
+  reverse: -1,
+};
+
+interface PostGridProps {
+  variant?: keyof typeof gridVariants;
+}
+
+const PostsGrid: FunctionalComponent<PostGridProps> = (
+  { children, variant = 'default' },
+) => {
   // Modify first post title size
   const cssGrid = tw(
     css({
       '& a:first-child h2': {
         '@apply': 'md:text-[32px]',
       },
+      '& a:first-child div:nth-child(2)': {
+        order: gridVariants[variant],
+      },
     }),
   );
   return (
-    <div class={`grid grid-cols(1 md:3) gap-10 ${cssGrid}`}>
+    <div class={`grid grid-cols(1 md:3) gap(4 md:x-10 md:y-8) ${cssGrid}`}>
       {children}
     </div>
   );
