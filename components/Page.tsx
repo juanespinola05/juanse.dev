8dae89eb13efd23dc86f79796471310b080720e8
Juan Sebastian Espinola <sebar5er@gmail.com>
2023-01-15 17:57:15 -0300

    add background effect

--- components/Page.tsx
+++ components/Page.tsx
@@ -1,8 +1,26 @@
 import { FunctionalComponent } from 'preact';
+import { tw } from 'twind';
+import { css } from 'twind/css';
 
 const Page: FunctionalComponent = ({ children }) => {
+  const backgroundCSS = tw(css({
+    background: 'linear-gradient(45deg, #E7227E, #01011d, #010137)',
+    backgroundSize: '400% 400%',
+    animation: 'gradient 15s ease infinite',
+    '@keyframes gradient': {
+      '0%': {
+        backgroundPosition: '0% 50%',
+      },
+      '50%': {
+        backgroundPosition: '100% 50%',
+      },
+      '100%': {
+        backgroundPosition: '0% 50%',
+      },
+    },
+  }));
   return (
-    <div class='min-h-screen bg-gradient-to-r from-black to-darkBlue text-white font-oswald'>
+    <div class={`min-h-screen ${backgroundCSS} text-white font-oswald`}>
       {children}
     </div>
   );
