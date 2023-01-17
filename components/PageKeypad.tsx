e021e86a076ea0358b6118f85c60f884ff24def2
Juan Sebastian Espinola <sebar5er@gmail.com>
2023-01-17 00:22:03 -0300

    create keypad for pagination

---
+++ components/PageKeypad.tsx
@@ -0,0 +1,30 @@
+import { FunctionalComponent } from 'https://esm.sh/v102/preact@10.11.0/src/index';
+import { PostsPagination } from '../utils/post.ts';
+
+interface PageKeyPadProps extends Omit<PostsPagination, 'posts'> {}
+
+const PageKeypad: FunctionalComponent<PageKeyPadProps> = (
+  { currentPage, totalPages },
+) => {
+  const pageButtons = Array.from({ length: totalPages }, (_, i) => i + 1);
+  return (
+    <div class='h-16 mb-12 flex justify-center items-center gap-2 flex-wrap'>
+      {pageButtons.map((page) => {
+        const isCurrent = currentPage === page;
+        const path = isCurrent ? '#' : `/blog/page/${page}`;
+        return (
+          <a
+            href={path}
+            class={`block px-3 py-2 bg-pink shadow-md ${
+              isCurrent ? 'bg-transparent border-pink border-2' : ''
+            }`}
+          >
+            {page}
+          </a>
+        );
+      })}
+    </div>
+  );
+};
+
+export default PageKeypad;
--- routes/blog/page/[page].tsx
+++ routes/blog/page/[page].tsx
@@ -2,10 +2,9 @@ import { Handlers, PageProps } from '$fresh/server.ts';
 import BaseBody from '../../../components/BaseBody.tsx';
 import BaseHead from '../../../components/BaseHead.tsx';
 import Container from '../../../components/Container.tsx';
+import PageKeypad from '../../../components/PageKeypad.tsx';
 import PostComponent from '../../../components/Post.tsx';
-import PostsGrid from '../../../components/PostsGrid.tsx';
 import Title from '../../../components/Title.tsx';
-import { Post } from '../../../types/posts.d.ts';
 import { loadPostsByPage, PostsPagination } from '../../../utils/post.ts';
 
 export const handler: Handlers = {
@@ -29,28 +28,12 @@ export default function BlogPage(props: PageProps<PostsPagination>) {
         <title>Juanse | Blog</title>
       </BaseHead>
       <BaseBody pathname='/blog'>
-        <div class='mt-12'>
-          <Container>
-            <div class='my-12'>
-              <Title size='5xl'>BLOG</Title>
-            </div>
-            <PostsGrid>
-              {posts.map((post) => <PostComponent {...post} />)}
-            </PostsGrid>
-            <div class='h-12 flex justify-center items-center gap-4'>
-              <div id='previous'>
-                {currentPage > 1 && (
-                  <a href={`/blog/page/${currentPage - 1}`}>Anterior</a>
-                )}
-              </div>
-              <div id='next'>
-                {currentPage < totalPages && (
-                  <a href={`/blog/page/${currentPage + 1}`}>Siguiente</a>
-                )}
-              </div>
-            </div>
-          </Container>
-        </div>
+        <Container>
+          <div class='my-12 grid grid-cols(1 md:2 lg:3) gap-6'>
+            {posts.map((post) => <PostComponent {...post} />)}
+          </div>
+          <PageKeypad totalPages={totalPages} currentPage={currentPage} />
+        </Container>
       </BaseBody>
     </>
   );
