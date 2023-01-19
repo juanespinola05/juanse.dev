519d44d8d63aa8fc38d05460453838f53dc7b781
Juan Sebastian Espinola <sebar5er@gmail.com>
2023-01-19 16:53:01 -0300

    fix page title and types

--- routes/blog/[slug].tsx
+++ routes/blog/[slug].tsx
@@ -3,25 +3,25 @@ import { CSS } from 'deno-gfm';
 import BaseBody from '../../components/BaseBody.tsx';
 import BaseHead from '../../components/BaseHead.tsx';
 import Container from '../../components/Container.tsx';
+import { Post } from '../../types/posts.d.ts';
 import { loadPost } from '../../utils/post.ts';
 
 export const handler: Handlers = {
   async GET(request, context): Promise<Response> {
     const { slug } = context.params;
     const post = await loadPost(slug);
-    console.log(post);
-    if (!post) return context.renderNotFound();
 
+    if (!post) return context.renderNotFound();
     return context.render({ post });
   },
 };
 
-export default function PagePost(props: PageProps) {
+export default function PagePost(props: PageProps<{ post: Post }>) {
   const { post } = props.data;
   return (
     <>
       <BaseHead>
-        <title>Juanse | 404</title>
+        <title>{post.title}</title>
       </BaseHead>
       <BaseBody>
         <Container>
