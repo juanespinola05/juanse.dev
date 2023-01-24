7315b3edbfef065a76af7227ebb6e9325c4a667a
Juan Sebastian Espinola <sebar5er@gmail.com>
2023-01-24 20:22:29 -0300

    improve page styles

--- routes/blog/[slug].tsx
+++ routes/blog/[slug].tsx
@@ -1,10 +1,15 @@
 import { Handlers, PageProps } from '$fresh/server.ts';
-import { CSS } from 'deno-gfm';
+import { css } from 'twind/css';
+import { tw } from 'twind';
 import BaseBody from '../../components/BaseBody.tsx';
 import BaseHead from '../../components/BaseHead.tsx';
 import Container from '../../components/Container.tsx';
 import { Post } from '../../types/posts.d.ts';
 import { loadPost } from '../../utils/post.ts';
+import PostNavigation from '../../islands/PostNavigations.tsx';
+// TODO: make this dinamic?
+import 'prismjs/components/prism-bash?no-check';
+import 'prismjs/components/prism-yaml?no-check';
 
 export const handler: Handlers = {
   async GET(request, context): Promise<Response> {
@@ -18,30 +23,45 @@ export const handler: Handlers = {
 
 export default function PagePost(props: PageProps<{ post: Post }>) {
   const { post } = props.data;
+  const date = Intl.DateTimeFormat('es').format(new Date(post.date));
+  const gridCss = tw(css({
+    'grid-template-columns': '100px auto',
+  }));
   return (
     <>
       <BaseHead>
         <title>{post.title}</title>
+        <link
+          rel='stylesheet'
+          type='text/css'
+          href='/md.css'
+        />
       </BaseHead>
       <BaseBody>
         <Container>
-          <article>
-            <time>
-              {Intl.DateTimeFormat('es').format(new Date(post.date))}
-            </time>
-            {/* <style dangerouslySetInnerHTML={{ __html: CSS }}></style> */}
-            <link
-              rel='stylesheet'
-              type='text/css'
-              href='/markdown2.css'
-            />
+          <article class='max-w-3xl mx-auto override:list-none'>
+            <div className='absolute right-[20px] top-52 hidden xl:block'>
+              <PostNavigation />
+            </div>
+            <div class={`grid ${gridCss} gap-3 my-12`}>
+              <div class='w-24 h-24'>
+                <img
+                  class='w-24 h-24'
+                  src={post.iconUrl}
+                  alt={post.title}
+                />
+              </div>
+              <div>
+                <h1 class='text-3xl'>{post.title}</h1>
+                <p class='mt-2 text-sm'>{date} &bull; {post.readingTime}</p>
+              </div>
+            </div>
             <div
               data-color-mode='dark'
               data-dark-theme='dark'
               dangerouslySetInnerHTML={{ __html: post.body }}
               class='markdown-body'
             >
-              {post.body}
             </div>
           </article>
         </Container>
