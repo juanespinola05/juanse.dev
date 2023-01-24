2a01e440ca05e808a5a527acac37ccd4469280a2
Juan Sebastian Espinola <sebar5er@gmail.com>
2023-01-24 19:00:11 -0300

    fix post page breaking

--- components/Container.tsx
+++ components/Container.tsx
@@ -2,7 +2,7 @@ import { FunctionalComponent } from 'preact';
 
 const Container: FunctionalComponent = ({ children }) => {
   return (
-    <div class='px-[15px] mx-auto md:w-[750px] lg:w-[970px] xl:w-[1170px]'>
+    <div class='max-w-[100vw] px-[15px] mx-auto md:w-[750px] lg:w-[970px] xl:w-[1170px]'>
       {children}
     </div>
   );
