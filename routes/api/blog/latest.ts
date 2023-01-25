f2190d9fa6ccc7696996a20778f8078705a3a9df
Juan Sebastian Espinola <sebar5er@gmail.com>
2023-01-25 09:19:58 -0300

    feat: add details query params

--- routes/api/blog/latest.ts
+++ routes/api/blog/latest.ts
@@ -2,12 +2,22 @@ import { Handlers } from '$fresh/server.ts';
 import { loadPosts } from '../../../utils/post.ts';
 
 export const handler: Handlers = {
-  async GET(): Promise<Response> {
+  async GET(req): Promise<Response> {
     const posts = await loadPosts();
+    const reqURL = new URL(req.url);
+    const { searchParams } = reqURL;
+
+    const details = Boolean(searchParams.get('details')) ?? false;
+
     posts.sort((a, b) =>
-      new Date(a.date).getTime() - new Date(b.date).getTime()
+      new Date(b.date).getTime() - new Date(a.date).getTime()
     );
-    const latest = posts.slice(0, 3);
+    const latest = posts
+      .slice(0, 3)
+      .map((entry) => ({
+        ...entry,
+        body: details ? entry.body : null,
+      }));
     const response = new Response(JSON.stringify({ posts: latest }), {
       headers: {
         'Content-Type': 'application/json',
