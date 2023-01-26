e9234c6c80f8683e291ede3416e1b38b4f704496
Juan Sebastian Espinola <sebar5er@gmail.com>
2023-01-26 13:54:55 -0300

    create share on twitter component

---
+++ components/ShareOnTwitter.tsx
@@ -0,0 +1,28 @@
+import IconTwitter from 'tabler-icon/brand-twitter.tsx';
+import { FunctionalComponent } from 'preact';
+
+interface ShareOnTwitterProps {
+  url: string;
+  text: string;
+}
+
+const ShareOnTwitter: FunctionalComponent<ShareOnTwitterProps> = (
+  { url, text },
+) => {
+  const shareUrl = new URL(
+    `https://twitter.com/intent/tweet?url=${url}&text=${text}`,
+  );
+  return (
+    <a
+      rel='noopener noreferrer'
+      target='_blank'
+      href={shareUrl.toString()}
+      class='cursor-pointer bg-white text-[#1D9BF0] rounded-xl bottom-10 right-10 p-3 fixed flex gap-2'
+      title='Compartir publicación en Twitter'
+    >
+      <IconTwitter />
+    </a>
+  );
+};
+
+export default ShareOnTwitter;
