207f89d4af0ac09857ad5ec0fa3afcd31f2d01d0
Juan Sebastian Espinola <sebar5er@gmail.com>
2023-01-26 11:37:22 -0300

    add author meta tag

--- components/BaseHead.tsx
+++ components/BaseHead.tsx
@@ -15,6 +15,7 @@ const BaseHead: FunctionalComponent = ({ children }) => {
         href='https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;700&display=swap'
         rel='stylesheet'
       />
+      <meta name='author' content='Juan Sebastian Espinola' />
     </Head>
   );
 };
