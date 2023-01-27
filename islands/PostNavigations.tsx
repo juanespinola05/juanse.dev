f692624466bfe507e1688508f40bb75951ab894d
Juan Sebastian Espinola <sebar5er@gmail.com>
2023-01-26 23:24:10 -0300

    fix post navigation hover

--- islands/PostNavigations.tsx
+++ islands/PostNavigations.tsx
@@ -15,9 +15,9 @@ const PostNavigation = () => {
       <p class='text-sm font-bold mb-2'>Contenido</p>
       <ul class='list-inside flex flex-col gap-1'>
         {sections.map((section) => (
-          <li key={section.id} class='hover:font-bold'>
+          <li key={section.id} class='text-gray-400 hover:text-white'>
             <a href={`#${section.id}`}>
-              <span class='text-gray-300'>- &nbsp;</span>
+              <span class='text-white'>- &nbsp;</span>
               {section.textContent}
             </a>
           </li>
