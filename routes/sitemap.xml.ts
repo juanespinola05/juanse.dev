fad1c1acbd52e851c70aaaca866a68cb1cf4bc89
Juan Sebastian Espinola <sebar5er@gmail.com>
2023-01-29 20:54:27 -0300

    add blog pages routes to sitemap

--- routes/sitemap.xml.ts
+++ routes/sitemap.xml.ts
@@ -9,6 +9,11 @@ export const handler: Handlers = {
     const posts = await loadPosts();
     posts.forEach(({ id }) => sitemap.add(`/blog/${id}`));
 
+    const totalPages = (posts.length - (posts.length % 6)) / 6;
+
+    for (let i = 1; i <= totalPages; i++) {
+      sitemap.add(`/blog/page/${i}`);
+    }
     sitemap.remove('/api/blog/latest');
     return sitemap.render();
   },
