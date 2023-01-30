ba201081c5cc094f4cc15c89d071da001b93a7b9
Juan Sebastian Espinola <sebar5er@gmail.com>
2023-01-30 14:26:49 -0300

    create social media types and share options

--- types.d.ts
+++ types.d.ts
@@ -1,3 +1,12 @@
 export interface PathnameProps {
   pathname?: string;
 }
+
+export type SocialMediaTypes =
+  | 'facebook'
+  | 'twitter'
+  | 'whatsapp'
+  | 'linkedin'
+  | 'telegram';
+
+export interface ShareOptions extends Omit<ShareData, 'files'> {}
