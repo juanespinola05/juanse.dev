98a83611b028b967a7c75bb23fe47daf9a3e238e
Juan Sebastian Espinola <sebar5er@gmail.com>
2023-01-30 14:27:07 -0300

    create social media share links

---
+++ components/SocialMediaShareList.tsx
@@ -0,0 +1,49 @@
+import { FunctionalComponent } from 'preact';
+import { ShareOptions, SocialMediaTypes } from '../types.d.ts';
+import SocialMediaIcon from './SocialMediaIcon.tsx';
+
+const SocialMediaShareList: FunctionalComponent<ShareOptions> = (
+  { url, text },
+) => {
+  const socialMediaList = [
+    {
+      name: 'facebook',
+      link: `https://www.facebook.com/sharer/sharer.php?u=${url}`,
+    },
+    {
+      name: 'twitter',
+      link: `https://twitter.com/intent/tweet?url=${url}&text=${text}`,
+    },
+    {
+      name: 'whatsapp',
+      link: `whatsapp://send?text=${text}\n${url}`,
+    },
+    {
+      name: 'telegram',
+      link: `https://t.me/share/url?url=${url}&text=${text}`,
+    },
+    {
+      name: 'linkedin',
+      link: `https://www.linkedin.com/shareArticle?mini=true&url=${url}`,
+    },
+  ];
+  return (
+    <ul class='flex overflow-x-scroll gap-3 py-2'>
+      {socialMediaList.map((media) => (
+        <li key={media.name}>
+          <a
+            target='_blank'
+            rel='nooopener noreferrer'
+            href={media.link}
+          >
+            <SocialMediaIcon
+              socialMedia={media.name as SocialMediaTypes}
+            />
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default SocialMediaShareList;
