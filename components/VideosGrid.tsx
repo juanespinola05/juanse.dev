1fda718709a5d1237531a7bbcb28fb50d95bd8fe
Juan Sebastian Espinola <sebar5er@gmail.com>
2023-02-02 18:25:19 -0300

    move videos list map here

--- components/VideosGrid.tsx
+++ components/VideosGrid.tsx
@@ -1,10 +1,27 @@
 import { FunctionalComponent } from 'preact';
+import { VideoDetails } from '../types/videos.d.ts';
+import Video from './Video.tsx';
 
-const VideosGrid: FunctionalComponent = ({ children }) => {
+interface VideosGridProps {
+  videos: VideoDetails[];
+  displayNumber: number;
+  moreVideosPath?: string;
+}
+
+const VideosGrid: FunctionalComponent<VideosGridProps> = (
+  { displayNumber, videos, moreVideosPath = '/videos' },
+) => {
   return (
-    <div className='grid grid-cols(1 lg:2) gap-5 justify-items-center'>
-      {children}
-    </div>
+    <>
+      <div className='grid grid-cols(1 lg:2) gap-5 justify-items-center'>
+        {videos.filter((_, i) => i < displayNumber).map((video) => (
+          <Video {...video} />
+        ))}
+      </div>
+      <div className='flex justify-end mt-5'>
+        <a href={moreVideosPath} class='text-right text-blue-500'>Ver más</a>
+      </div>
+    </>
   );
 };
 
