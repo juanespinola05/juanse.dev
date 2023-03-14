ceecf36a2938ccbe2205ce8407e28ea430665a21
Juan Sebastian Espinola <sebar5er@gmail.com>
2023-03-14 20:43:51 -0300

    Update fonts and color palette

--- layouts/Head.tsx
+++ layouts/Head.tsx
@@ -6,13 +6,9 @@ const Head: FunctionalComponent = ({ children }) => {
     <RuntimeHead>
       {children}
       <link rel='preconnect' href='https://fonts.googleapis.com' />
+      <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='' />
       <link
-        rel='preconnect'
-        href='https://fonts.gstatic.com'
-        crossOrigin='true'
-      />
-      <link
-        href='https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;700&display=swap'
+        href='https://fonts.googleapis.com/css2?family=Oswald:wght@400;700&family=Source+Sans+Pro:wght@400;700&display=swap'
         rel='stylesheet'
       />
       <meta name='author' content='Juan Sebastian Espinola' />
--- twind.config.ts
+++ twind.config.ts
@@ -1,6 +1,6 @@
 import { Options } from '$fresh/plugins/twind.ts'
 import { css } from 'twind/css'
-import { apply, tw } from 'twind'
+import { apply } from 'twind'
 
 /** @type {Omit<import("$fresh/plugins/twind.ts").Options, "selfURL">} */
 export default {
@@ -41,20 +41,15 @@ export default {
       colors: {
         darkBlue: '#010137',
         pink: '#E71E7C',
+        moonstone: '#6db1bfff',
+        trueblue: '#4062bbff',
+        mimipink: '#f2cee6ff',
+        base: 'hsl(220 17.647% 20%)',
       },
       fontFamily: {
         'oswald': ['Oswald', 'Helvetica', 'sans-serif'],
         'default': [
-          'system-ui',
-          '-apple-system',
-          'BlinkMacSystemFont',
-          'Segoe UI',
-          'Roboto',
-          'Oxygen',
-          'Ubuntu',
-          'Cantarell',
-          'Open Sans',
-          'Helvetica Neue',
+          'Source Sans Pro',
           'sans-serif',
         ],
       },
